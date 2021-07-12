4f612d7664d2a6dec826dc57158a6b0084a0549c
aplaceof <aplaceof@163.com>
2021-07-12 16:55:20 +0800

    sdf

---
+++ src/main/Test.java
@@ -0,0 +1,25 @@
+package main;
+
+import java.util.ArrayList;
+
+/**
+ * class Test @Title this is a class
+ *
+ * @author alf
+ */
+public class Test {
+
+  /** b */
+  int a;
+  /** b */
+  int b;
+
+  /**
+   * this is a function
+   *
+   * @param a a
+   */
+  public void fun(int a) {
+    ArrayList<Integer> arr = new ArrayList<>();
+  }
+}
